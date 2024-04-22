3634fa2ea2acd3e117ca1dc93563c31a81e6601c
Hussein380 <huznigarane@gmail.com>
2024-04-22 18:21:38 +0300

    using puppet to install and configure Nginx server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,27 @@
+# Update system package lists
+exec { 'update system':
+  command => '/usr/bin/apt-get update',
+}
+
+# Install Nginx package
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update system'],
+}
+
+# Create a simple HTML file
+file {'/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Configure Nginx to redirect /redirect_me to a YouTube video
+exec {'redirect_me':
+  command  => 'sed -i "24i\    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+# Ensure Nginx service is running
+service {'nginx':
+  ensure => running,
+  require => Package['nginx'],
+}
