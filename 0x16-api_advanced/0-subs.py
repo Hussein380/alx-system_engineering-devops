f9f41c08bea4becd7ea24826f5e63393ffd48db3
Hussein380 <huznigarane@gmail.com>
2024-06-19 20:18:59 +0300

    pep 8 recommendation correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-     Queries the Reddit API and returns the number of subscribers for a given subreddit.
+     Queries the Reddit API and returns the number of subscribers
+     for a given subreddit.
      If an invalid subreddit is given, returns 0.
         :param subreddit: Subreddit name to query.
         :return: Number of subscribers, or 0 if subreddit is invalid.
     """
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {'User-Agent': 'MyRedditScript/0.1'}
-    #Make API Request
+    # Make API Request
     try:
         # Make API requests
         response = requests.get(url, headers=headers, allow_redirects=False)
-        #check if the response status vode is 200 (ok)
+        # check if the response status vode is 200 (ok)
         if response.status_code != 200:
             return 0
         # Parse the json response to get the number of subscribers
@@ -23,5 +25,3 @@ def number_of_subscribers(subreddit):
         return subscribers
     except requests.RequestException:
         return 0
-
-
